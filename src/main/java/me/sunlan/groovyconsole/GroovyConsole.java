bb299b56a50f2d876753cedd5ff1ca94399e0df0
danielsun1106 <realbluesun@hotmail.com>
2017-07-08 15:41:07 +0800

    Simplify the example

--- src/main/java/me/sunlan/groovyconsole/GroovyConsole.java
+++ src/main/java/me/sunlan/groovyconsole/GroovyConsole.java
@@ -2,14 +2,12 @@ package me.sunlan.groovyconsole;
 
 import groovy.lang.Binding;
 import groovy.ui.Console;
-import org.codehaus.groovy.control.CompilerConfiguration;
 
 /**
  * Created by Daniel on 2017/7/8.
  */
 public class GroovyConsole {
     public static void main(String[] args) {
-        CompilerConfiguration configuration = new CompilerConfiguration(CompilerConfiguration.DEFAULT);
-        new Console(GroovyConsole.class.getClassLoader(), new Binding(), configuration).run();
+        new Console(GroovyConsole.class.getClassLoader(), new Binding()).run();
     }
 }
